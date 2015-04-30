134332a411e8f750a3a45b57035ec2b8e6a91f01
Sarah Mei <sarahmei@gmail.com>
2015-04-30 16:18:22 -0400

    Make perform_match more understandable

--- lib/rspec/matchers/built_in/include.rb
+++ lib/rspec/matchers/built_in/include.rb
@@ -60,53 +60,120 @@ module RSpec
         def perform_match(predicate, hash_subset_predicate)
           return false unless actual.respond_to?(:include?)
 
+          inclusion_checker = inclusion_checker(hash_subset_predicate)
           if Array === actual
-            count = ExpectedActualPairingSolver.best_solution(
-              expected,
-              actual
-            ).unmatched_expected_indices.count
-
             if predicate == :none?
-              count == expected.size
+              inclusion_checker.call(0)
             elsif predicate == :all?
-              count == 0
-            end
-          elsif Hash === actual
-            expected.__send__(predicate) do |expected_item|
-              if Hash === expected_item
-                expected_item.__send__(hash_subset_predicate) do |(key, value)|
-                  actual_hash_includes?(key, value)
-                end
-              else
-                actual_hash_has_key?(expected_item)
-              end
+              inclusion_checker.call(expected.size)
             end
           else
-            expected.__send__(predicate) do |expected_item|
-              actual_collection_includes?(expected_item)
+            if predicate == :none?
+              expected.none? { |expected_item| inclusion_checker.call(expected_item) }
+            elsif predicate == :all?
+              expected.all? { |expected_item| inclusion_checker.call(expected_item) }
             end
           end
         end
 
-        def actual_hash_includes?(expected_key, expected_value)
-          actual_value = actual.fetch(expected_key) { return false }
-          values_match?(expected_value, actual_value)
+        def inclusion_checker(hash_subset_predicate)
+          case actual
+            when Array
+              ArrayInclusionChecker.new(expected, actual)
+            when Hash
+              HashInclusionChecker.new(expected, actual, hash_subset_predicate)
+            else
+              CollectionInclusionChecker.new(expected, actual)
+          end
         end
 
-        def actual_hash_has_key?(expected_key)
-          # We check `key?` first for perf:
-          # `key?` is O(1), but `any?` is O(N).
-          actual.key?(expected_key) ||
-          actual.keys.any? { |key| values_match?(expected_key, key) }
+        class ArrayInclusionChecker
+          def initialize(expected, actual)
+            @expected = expected
+            @actual = actual
+          end
+
+          def call(expected_matching_item_count)
+            unmatched_item_count = ExpectedActualPairingSolver.best_solution(
+              expected,
+              actual
+            ).unmatched_expected_indices.count
+            expected.count - unmatched_item_count == expected_matching_item_count
+          end
+
+          protected
+
+          attr_reader :expected, :actual
         end
 
-        def actual_collection_includes?(expected_item)
-          return true if actual.include?(expected_item)
+        class HashInclusionChecker
+          def initialize(expected, actual, hash_subset_predicate)
+            @expected = expected
+            @actual = actual
+            @hash_subset_predicate = hash_subset_predicate
+          end
 
-          # String lacks an `any?` method...
-          return false unless actual.respond_to?(:any?)
+          def call(expected_item)
+            if Hash === expected_item
+              expected_item.__send__(hash_subset_predicate) do |(key, value)|
+                actual_hash_includes?(key, value)
+              end
+            else
+              actual_hash_has_key?(expected_item)
+            end
+          end
 
-          actual.any? { |value| values_match?(expected_item, value) }
+        protected
+
+          attr_reader :expected, :actual, :hash_subset_predicate
+
+        private
+
+          def actual_hash_includes?(expected_key, expected_value)
+            actual_value = actual.fetch(expected_key) { return false }
+            values_match?(expected_value, actual_value)
+          end
+
+          def actual_hash_has_key?(expected_key)
+            # We check `key?` first for perf:
+            # `key?` is O(1), but `any?` is O(N).
+            actual.key?(expected_key) ||
+              actual.keys.any? { |key| values_match?(expected_key, key) }
+          end
+
+          def values_match?(expected, actual)
+            RSpec::Matchers::Composable.values_match?(expected, actual)
+          end
+        end
+
+        class CollectionInclusionChecker
+          def initialize(expected, actual)
+            @expected = expected
+            @actual = actual
+          end
+
+          def call(expected_item)
+            actual_collection_includes?(expected_item)
+          end
+
+          protected
+
+          attr_reader :expected, :actual
+
+          private
+
+          def actual_collection_includes?(expected_item)
+            return true if actual.include?(expected_item)
+
+            # String lacks an `any?` method...
+            return false unless actual.respond_to?(:any?)
+
+            actual.any? { |value| values_match?(expected_item, value) }
+          end
+
+          def values_match?(expected, actual)
+            RSpec::Matchers::Composable.values_match?(expected, actual)
+          end
         end
       end
     end
